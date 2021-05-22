52ead5a008922c1c1a8bf281056a517c98f0093f
brobbins2001 <bryanadmin@localhost>
2021-05-22 15:56:02 -0400

    Added an example Block

---
+++ src/main/java/net/mcreator/thewildcatmod/block/ExampleWildcatBlockBlock.java
@@ -0,0 +1,49 @@
+
+package net.mcreator.thewildcatmod.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.thewildcatmod.ThewildcatmodModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@ThewildcatmodModElements.ModElement.Tag
+public class ExampleWildcatBlockBlock extends ThewildcatmodModElements.ModElement {
+	@ObjectHolder("thewildcatmod:example_wildcat_block")
+	public static final Block block = null;
+	public ExampleWildcatBlockBlock(ThewildcatmodModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0));
+			setRegistryName("example_wildcat_block");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
